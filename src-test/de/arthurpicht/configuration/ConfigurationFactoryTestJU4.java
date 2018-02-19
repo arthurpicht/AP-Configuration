8bf8ed6b80d99832f8e7cfce350e964e357d865c
Arthur Picht <picht@arthurpicht.de>
2018-02-19 22:01:19 +0100

    preparing tests

---
+++ src-test/de/arthurpicht/configuration/ConfigurationFactoryTestJU4.java
@@ -0,0 +1,18 @@
+package de.arthurpicht.configuration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConfigurationFactoryTestJU4 {
+
+    @Test
+    public void addConfigurationFromFilesystem() {
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void addConfigurationFromClasspath() {
+        System.out.println("Test2");
+    }
+}
